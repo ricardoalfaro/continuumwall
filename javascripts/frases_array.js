cbaef7f99b691703c13a61fe3af4ccf27462e2e5
Ricardo Alfaro <ricardo.alfaro@continuum.cl>
2012-02-16 20:06:42 -0300

    se avanza en el diseño y se agrega script para random de frases y tooltips

---
+++ javascripts/frases_array.js
@@ -0,0 +1,45 @@
+cl = {};
+cl.continuum = {};
+
+cl.continuum.wall = {
+
+    RANDOM_FRASES : 10000,
+
+    frasesWall : [
+        {
+            texto : "Estoy más duro que la chucha",
+            autor : "Mauro"
+        },
+        {
+            texto : "Se la dejé puesta",
+            autor : "Abraham a Jano"
+        },
+
+        {
+            texto : "Es como un temblor, pero en ti",
+            autor : "Victor (El dostor)"
+        },
+
+        {
+            texto : "Pero lo bueno es que es chico",
+            autor : "Rodrigo Ayala"
+        },
+
+        {
+            texto : "Si quieres una wea sensible, entonces consíguete una mina",
+            autor : "Victor (El dostor)"
+        },
+
+        {
+            texto : "Mañana te lo traigo calientito",
+            autor : "Ricardo Jara"
+        },
+
+        {
+            texto : "Hay que mostrar los huevos",
+            autor : "Ricardo Jara"
+        }
+
+    ]
+
+}
--- javascripts/random-frases.js
+++ javascripts/random-frases.js
@@ -1,10 +1,58 @@
-$(function(){
-  $('#highlight').click(function(){
-    // Remove existing highlights
-    $('.highlighted').removeClass('highlighted');
-    
-    // Get selector and apply .hightlighted
-    var selector = $('#selector').val();
-    $(selector).addClass('highlighted');
-  })
-})
+(function ($) {
+
+var last_phrase = -1;
+
+function showFrases(id, animate) {
+  var frase = cl.continuum.wall.frasesWall[id];
+  // fadeIn, fadeOut de la frase
+  var p_frase = $('h1#frase_completa')
+  var p_autor = $('h2#frase_autor')
+		if (animate) {
+            p_frase.fadeOut(2000, function () {
+                $(this).empty().append(frase.texto).fadeIn(2000);
+            })
+            p_autor.fadeOut(2000, function () {
+                $(this).empty().append(frase.autor).fadeIn(2000);
+            })
+        } else {
+            p_frase.empty().append(frase.texto);
+            p_autor.empty().append(frase.autor)
+        }
+    }
+
+
+
+function _random(tope, last_one) {
+  var rand = (Math.floor(Math.random()*tope));
+  return (rand != last_one ? rand : _random(tope, last_one));
+ }
+
+ var intervalID = 0;
+
+ function randomFrases() {
+  intervalID = setInterval(function () {
+  var rd = _random(cl.continuum.wall.frasesWall.length, last_phrase);
+     showFrases(rd, true)
+     last_phrase = rd;
+    }, cl.continuum.wall.RANDOM_FRASES);
+  }
+
+  $(document).ready(function () {
+        // cargar una frase aleatoria
+        showFrases(_random(cl.continuum.wall.frasesWall.length))
+
+        // mostrar mas frases
+        $('.otrafrase').click(function (e) {
+          var rd = _random(cl.continuum.wall.frasesWall.length, last_phrase);
+          showFrases(rd);
+          last_phrase = rd;
+          e.preventDefault();
+        });
+
+        /*$('.otrafrase').click(function() {
+          showFrases(_random(cl.continuum.wall.frasesWall.length))
+        });*/
+
+    });
+
+})(jQuery)
