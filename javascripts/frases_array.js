7634504f1b4aea3b5647100c500a85b38c2fa7c5
Ricardo Alfaro <ricardo.alfaro@continuum.cl>
2012-02-17 18:10:57 -0300

    se cambia fondo, se corrigen defectos css y se agregan mas frases al arreglo

--- javascripts/frases_array.js
+++ javascripts/frases_array.js
@@ -10,6 +10,7 @@ cl.continuum.wall = {
             texto : "Estoy m&aacute;s duro que la chucha",
             autor : "Mauro"
         },
+
         {
             texto : "Se la dej&eacute; puesta",
             autor : "Abraham a Jano"
@@ -38,6 +39,161 @@ cl.continuum.wall = {
         {
             texto : "Hay que mostrar los huevos",
             autor : "Ricardo Jara"
+        },
+
+        {
+            texto : "S&aacute;calo todo",
+            autor : "Jano a Jorge"
+        },
+
+        {
+            texto : "P&aacute;sala no m&aacute;s... suavecita",
+            autor : "Mauro"
+        },
+
+        {
+            texto : "Mauro est&aacute; tampoco",
+            autor : "Leo"
+        },
+
+        {
+            texto : "Mis manos trabajan mucho m&aacute;s lento que tu boca",
+            autor : "Jorge"
+        },
+
+        {
+            texto : "Nooo, si la mia es cortita",
+            autor : "Camilo a Rodrigo"
+        },
+
+        {
+            texto : "¿Que lado te gusta?",
+            autor : "Abraham a Jano"
+        },
+
+        {
+            texto : "Buena clav&aacute; ah?",
+            autor : "Mauro a Jano"
+        },
+
+        {
+            texto : "El Mauro mi bestia negra",
+            autor : "Jano"
+        },
+
+        {
+            texto : "Oh, que es chiquitito!",
+            autor : "Mar&iacute;a Jos&eacute; a Victor"
+        },
+
+        {
+            texto : "Cuando me caliento, juego!",
+            autor : "Rodrigo a Coto"
+        },
+
+        {
+            texto : "Es que cuando cambias de mano, igual se para!",
+            autor : "Rodrigo"
+        },
+
+        {
+            texto : "Abraham prodr&iacute;a presentar la herramienta",
+            autor : "Mauro"
+        },
+
+        {
+            texto : "El Alter me caga, pero rico!",
+            autor : "Mauro"
+        },
+
+        {
+            texto : "Yo la probar&iacute;a...",
+            autor : "Leo (a la herramienta de Abraham)"
+        },
+
+        {
+            texto : "¿Te lo presto?",
+            autor : "Abraham a Rodrigo"
+        },
+
+        {
+            texto : "¡En la mañana entra todo!",
+            autor : "Mauro a todos"
+        },
+
+        {
+            texto : "A lo mejor, te pod&iacute;s comer uno entero",
+            autor : "Victor (el dostor)"
+        },
+
+        {
+            texto : "Mmm, igual es grande!",
+            autor : "Camilo a Jose Alfredo"
+        },
+
+        {
+            texto : "¡Te pis&eacute; pero brigido!",
+            autor : "Mauro a Abraham"
+        },
+
+        {
+            texto : "¿Que apostamos?... Algo que no sea plata pos!",
+            autor : "Ricardo"
+        },
+
+        {
+            texto : "Si fueran de confianza...",
+            autor : "Alter"
+        },
+
+        {
+            texto : "Quiero que me mostris ese...",
+            autor : "Camilo a Rodrigo"
+        },
+
+        {
+            texto : "Ricardo, lo tuyo es chico",
+            autor : "Mauro"
+        },
+
+        {
+            texto : "Yo la puse ayer",
+            autor : "Ricardo"
+        },
+
+        {
+            texto : "Yo la tengo en el piso",
+            autor : "Victor"
+        },
+
+        {
+            texto : "Yo lo quiero completo",
+            autor : "Edu"
+        },
+
+        {
+            texto : "Te dej&oacute; paradito la pastilla ah?",
+            autor : "Rodrigo a Abraham"
+        },
+
+        {
+            texto : "Quiero probarlo",
+            autor : "Edu"
+        },
+
+        {
+            texto : "A mi me la cortaron",
+            autor : "Mauro"
+        },
+
+        {
+            texto : "¡Te amo!",
+            autor : "Sven a Edu"
+        },
+
+        {
+            texto : "¡Qued&eacute; en cuatro! ... A todos nos pasa alguna vez",
+            autor : "Camilo"
         }
 
     ]
